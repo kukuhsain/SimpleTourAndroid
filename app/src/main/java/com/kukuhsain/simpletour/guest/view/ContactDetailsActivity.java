c7ee8c3b0622555604abfa5a5f70f7cdbc09fd79
Kukuh <kukuhsain@gmail.com>
2016-10-22 23:14:00 +0700

    Add ContactDetailsActivity which can be accessed via book button from PackagesActivity

---
+++ app/src/main/java/com/kukuhsain/simpletour/guest/view/ContactDetailsActivity.java
@@ -0,0 +1,19 @@
+package com.kukuhsain.simpletour.guest.view;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.kukuhsain.simpletour.guest.R;
+
+/**
+ * Created by kukuh on 22/10/16.
+ */
+
+public class ContactDetailsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_details);
+    }
+}
--- app/src/main/java/com/kukuhsain/simpletour/guest/view/ReservationActivity.java
+++ app/src/main/java/com/kukuhsain/simpletour/guest/view/ReservationActivity.java
@@ -1,10 +1,14 @@
 package com.kukuhsain.simpletour.guest.view;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 import com.kukuhsain.simpletour.guest.R;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by kukuh on 22/10/16.
@@ -15,5 +19,13 @@ public class ReservationActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reservation);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btn_book)
+    public void goToContactDetails() {
+        Intent intent = new Intent(this, ContactDetailsActivity.class);
+        /*intent.putExtra("destination", (new Gson()).toJson(destination));*/
+        startActivity(intent);
     }
 }
